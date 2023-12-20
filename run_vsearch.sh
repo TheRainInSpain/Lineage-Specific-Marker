a96a8eca13461e7b7cc80fc02e41dc55f398783a
TheRainInSpain <fengjinghangf@gmail.com>
2023-12-20 17:01:26 +1100

    revise command in bash file run_vsearch.sh

--- run_vsearch.sh
+++ run_vsearch.sh
@@ -10,8 +10,8 @@ cd filtered_reads_folder
 for fasta in *.fasta;
 do
     fn=${fasta%.fasta}
-    echo "clustering_id85 $fasta..."
-    /home/u7343217/vsearch_check/./vsearch --cluster_size $fasta --id 0.85 --iddef 4 --centroids $vsearch_output_folder/centroids_fasta/$fn.fasta --uc $vsearch_output_folder/uc_files/$fn.uc --consout $vsearch_output_folder/consensus_fasta/$fn.fasta
+    echo "clustering $fasta..."
+    vsearch --cluster_size $fasta --id 0.85 --iddef 4 --centroids $vsearch_output_folder/centroids_fasta/$fn.fasta --uc $vsearch_output_folder/uc_files/$fn.uc --consout $vsearch_output_folder/consensus_fasta/$fn.fasta
     
 done
 
